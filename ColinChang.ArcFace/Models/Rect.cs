756eb2e3eff4a401d645c93bf3f3adf23e25c08f
Colin <zhangcheng5468@gmail.com>
2022-01-29 18:52:57 +0800

    1.修复头像坐标错误 2.升级Sample到.net 6.0

--- ColinChang.ArcFace/Models/Rect.cs
+++ ColinChang.ArcFace/Models/Rect.cs
@@ -5,9 +5,9 @@
     /// </summary>
     public struct Rect
     {
-        public int Top { get; set; }
-        public int Bottom { get; set; }
         public int Left { get; set; }
+        public int Top { get; set; }
         public int Right { get; set; }
+        public int Bottom { get; set; }
     }
 }
